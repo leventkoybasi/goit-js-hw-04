66ccdf2dc00508b7b0fda92954eb825115ed6196
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2024-09-06 13:25:31 -0700

    task3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(saatler) {
+    this.playTime += saatler;
+  },
+  getInfo() {
+    return `${profile.username} has ${profile.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
